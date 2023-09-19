4fd0d34480b1313648dd3a1288156801a449033a
Drunin <damian-201110@hotmail.com>
2023-09-19 17:16:45 -0500

    Cambios para selecionar las plantillas y se guarden, que solo se selecione una plantilla

--- src/IconModal.js
+++ src/IconModal.js
@@ -7,6 +7,9 @@ const IconModal = ({ onClose }) => {
   const contentRef = useRef(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [disabled, setDisabled] = useState(true);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+  const [selectedImagePath, setSelectedImagePath] = useState(null); 
+  const path = "";
 
   const handleFileUpload = () => {
     fileInput.current.click();
@@ -21,10 +24,47 @@ const IconModal = ({ onClose }) => {
     }
   };
 
+  const handleFileSelection = (path) => {
+    const img = new Image();
+  
+    img.onload = () => {
+      setSelectedImage(path);
+      setDisabled(false);
+    };
+  
+    img.onerror = () => {
+      console.error("Error al cargar la imagen desde la ruta:", path);
+    };
+  
+    img.src = path;
+   
+  };
+
+
+  const toggleImageSelection = (index) => {
+    if (selectedImageIndex === index) {
+      // Si se hace clic en la imagen ya seleccionada, deselecciónala
+      setSelectedImageIndex(null);
+    } else {
+      // Si se hace clic en una imagen diferente, actualiza la selección
+      setSelectedImageIndex(index);
+      handleImageClick(index);
+    }
+  };
+
+  // Llama a handleFileSelection con la imagen seleccionada
+  const handleImageClick = (index) => {
+    const selectedImage = images[index];
+    setSelectedImageIndex(index);
+    setSelectedImagePath(selectedImage); 
+    handleFileSelection(selectedImage.src);
+  };
+
   const handleSelectImage = () => {
+
     console.log(selectedImage)
   };
-  
+
 
   useEffect(() => {
     // Obtener la lista de imágenes de la carpeta "images"
@@ -112,60 +152,71 @@ const IconModal = ({ onClose }) => {
               }}
             >
               {images.length > 0 ? (
-                images.map((image, index) => (
+              images.map((image, index) => (
+                <div
+                  key={index}
+                  style={{
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                    margin: '5px',
+                    width: '150px',
+                    height: '150px',
+                    border: selectedImageIndex === index ? '2px solid blue' : 'none',
+                  }}
+                  onClick={() => toggleImageSelection(index)}
+                >
                   <img
-                    key={index}
                     src={image}
                     alt={`Imagen ${index}`}
                     style={{
                       maxWidth: '100%',
                       maxHeight: '100%',
-                      margin: '5px',
-                      width: '150px', // Ancho máximo de cada imagen
-                      height: '150px',
+                      width: '100%',
+                      height: '100%',
                     }}
                   />
-                ))
-              ) : (
-                <p>No hay plantillas</p>
-              )}
+                </div>
+              ))
+            ) : (
+              <p>No hay plantillas</p>
+            )}
             </div>
           ) : (
             <>
               {selectedImage ? (
                 <>
                   <img src={selectedImage}
-                  style={{
-                    width: '150px',
-                    height: '150px'
-                  }}/>
-                  <button type='button' 
-                  onClick={() => {
-                    setSelectedImage(null);
-                    setDisabled(true);
-                  }} 
-                  style={{
-                    fontSize: '16px',
-                    padding: '5px 9px',
-                    border: '1px solid #bababa',
-                    borderRadius: '0.25rem',
-                    marginTop: '10px',
-                    backgroundColor: '#F1F1F1',
-                    cursor: 'pointer',
-                    transition: 'background-color 0.5s ease',
-                  }}
-                  onMouseOver={(e) => {
-                    e.currentTarget.style.backgroundColor = '#d9d9d9';
-                  }}
-                  onMouseOut={(e) => {
-                    e.currentTarget.style.backgroundColor = '#F1F1F1';
-                  }}
+                    style={{
+                      width: '150px',
+                      height: '150px'
+                    }} />
+                  <button type='button'
+                    onClick={() => {
+                      setSelectedImage(null);
+                      setDisabled(true);
+                    }}
+                    style={{
+                      fontSize: '16px',
+                      padding: '5px 9px',
+                      border: '1px solid #bababa',
+                      borderRadius: '0.25rem',
+                      marginTop: '10px',
+                      backgroundColor: '#F1F1F1',
+                      cursor: 'pointer',
+                      transition: 'background-color 0.5s ease',
+                    }}
+                    onMouseOver={(e) => {
+                      e.currentTarget.style.backgroundColor = '#d9d9d9';
+                    }}
+                    onMouseOut={(e) => {
+                      e.currentTarget.style.backgroundColor = '#F1F1F1';
+                    }}
                   >
                     <img src="trash.png" alt="Eliminar"
-                    style={{
-                      width: '25px',
-                      height: '25px',
-                    }} /> 
+                      style={{
+                        width: '25px',
+                        height: '25px',
+                      }} />
                   </button>
                 </>
               ) : (
@@ -189,7 +240,7 @@ const IconModal = ({ onClose }) => {
                     e.currentTarget.style.backgroundColor = '#F1F1F1';
                   }}>
                   Seleccionar Icono</button>
-                  )}
+              )}
               <input type="file" accept=".png, .jpg, .jpeg" ref={fileInput} style={{ display: 'none' }} /></>
           )}
         </div>
